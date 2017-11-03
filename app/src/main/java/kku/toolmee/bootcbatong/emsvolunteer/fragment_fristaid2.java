ec6399b18614803020c74c9e3e3cb7c6c1692777
bootcba <ibootcba@gmail.com>
2017-11-03 13:42:18 +0700

    UI lablayuot frist aid

---
+++ app/src/main/java/kku/toolmee/bootcbatong/emsvolunteer/fragment_fristaid2.java
@@ -0,0 +1,29 @@
+package kku.toolmee.bootcbatong.emsvolunteer;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class fragment_fristaid2 extends Fragment {
+
+
+    public fragment_fristaid2() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fristaid2, container, false);
+    }
+
+}
--- app/src/main/java/kku/toolmee/bootcbatong/emsvolunteer/fristAid.java
+++ app/src/main/java/kku/toolmee/bootcbatong/emsvolunteer/fristAid.java
@@ -1,6 +1,8 @@
 package kku.toolmee.bootcbatong.emsvolunteer;
 
 import android.support.design.widget.TabLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -39,9 +41,6 @@ public class fristAid extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fristaid);
 
-
-
-
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         // Create the adapter that will return a fragment for each of the three
@@ -58,18 +57,26 @@ public class fristAid extends AppCompatActivity {
         tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
 
 
+        //Add back button
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+
+
     }
 
 
 
-    @Override
+     //setting on bar
+  /* @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_fristaid, menu);
+        getMenuInflater().inflate(R.menu.menu_frist_aid, menu);
         return true;
-    }
+    }*/
 
-    @Override
+    /*@Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
@@ -82,35 +89,22 @@ public class fristAid extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
+    }*/
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
+    //Add back button
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
 
-        public PlaceholderFragment() {
+        if (id == android.R.id.home){
+            this.finish();
         }
 
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-
+        return super.onOptionsItemSelected(item);
     }
+
+
+
 
     /**
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
@@ -126,13 +120,27 @@ public class fristAid extends AppCompatActivity {
         public Fragment getItem(int position) {
             // getItem is called to instantiate the fragment for the given page.
             // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+
+            switch (position){
+                case 0 :
+                    return new fragment_manuBoby();
+                case 1:
+                    return new fragment_fristaid1();
+                case 2:
+                    return new fragment_fristaid2();
+                case 3:
+                    return new fragment_fristaid3();
+                case 4:
+                    return new fragment_fristaid4();
+
+            }
+            return null;
         }
 
         @Override
         public int getCount() {
-            // Show 3 total pages.
-            return 4;
+            // Show 5 total pages.
+            return 5;
         }
     }
 }
